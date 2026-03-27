8bac6217d7f184ad513002e3a288ff5922705249
cogramer <hausmauswow@gmail.com>
2026-03-27 21:47:50 +0700

    Hoàn thành Book.java, BorrowRecord.java, Category.java, User.java trong folder model

--- library-management/src/main/java/com/library/model/BorrowRecord.java
+++ library-management/src/main/java/com/library/model/BorrowRecord.java
@@ -1,5 +1,43 @@
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!"); 
+package com.library.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "borrow_records")
+public class BorrowRecord {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "book_id", nullable = false)
+    private Book book;
+
+    @Column(name = "borrow_date", nullable = false)
+    private LocalDate borrowDate;
+
+    @Column(name = "due_date", nullable = false)
+    private LocalDate dueDate;
+
+    @Column(name = "return_date")
+    private LocalDate returnDate;
+
+    @Enumerated(EnumType.STRING)
+    private Status status = Status.BORROWING;
+
+    public enum Status {
+        BORROWING, RETURNED, OVERDUE
     }
 }
